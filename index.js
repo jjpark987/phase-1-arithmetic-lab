f188e5c833f90e2cf9fc923b17c27d38a8078ae1
Jason Park <jjpark987@gmail.com>
2022-10-12 14:01:34 -0700

    commit tree 9f84ff22f7eaa1c731ef30762297e3671dc232fb

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = 2;
 
-// Write your code here
+const num2 = 31;
+
+const num3 = 10;
+
+const num4 = 6;
+
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random() * 10);
+
+const mod = num3 % num4;
+
+const max = Math.max(20, 13, 7, 1, 19, 5, 3);
